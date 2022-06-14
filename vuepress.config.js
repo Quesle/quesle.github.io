d1f031cc5701fbcf4f5ddbb4ed68176fb026784b
Zhang Rui <zhangrui@growingio.com>
2022-06-14 19:51:23 +0800

    feat(blog): the docs of creating blog website

---
+++ vuepress.config.js
@@ -0,0 +1,22 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+
+module.exports = {
+  lang: "zh-CN",
+  title: "张小慕的博客",
+  description: "这是我的第一个 VuePress 站点",
+  theme: defaultTheme({
+    sidebar: [
+      {
+        text: "使用 Github Pages 搭建 Blog",
+        link: "/blog/",
+        children: [
+          "/blog/01.create_github_pages.md",
+          "/blog/02.create_blog_by_vuepress.md",
+          "/blog/03.config_vuepress.md",
+        ],
+      },
+      "/log",
+    ],
+  }),
+  dest: "./public",
+};
