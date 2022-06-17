9851ce49c3a3a168f3837800aee7f277a7488286
Zhang Rui <zhangrui@growingio.com>
2022-06-17 15:41:44 +0800

    fix: update docs

--- docs/assets/06.hooks.html.30951f34.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as _}from"./app.f46079ae.js";const c={};function r(t,s){return o(),_("div")}var n=e(c,[["render",r],["__file","06.hooks.html.vue"]]);export{n as default};
---
+++ docs/assets/06.hooks.html.5c49bd11.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as _}from"./app.cd6264c1.js";const c={};function r(t,s){return o(),_("div")}var n=e(c,[["render",r],["__file","06.hooks.html.vue"]]);export{n as default};
